d592167a85fcbb6dc849d72444765e73d64bec26
mitchelljaaron-stack <mitchelljaaron@gmail.com>
2025-10-20 21:07:57 -0600

    K Nearest Neighbors Predictions

--- Amazon_KNN.R
+++ Amazon_KNN.R
@@ -1,4 +1,4 @@
-# Logistic Using Random Forests
+# Amazon Predictions Using K Nearest Neighbors
 
 library(glmnet)
 
@@ -31,11 +31,10 @@ train_data <- train_data %>%
 test_data <- test_data %>%
   mutate(across(where(is.numeric), as.factor))
 
-rf_mod <- rand_forest(mtry = tune(),
-                      min_n=tune(),
-                      trees=500) %>%
-set_engine("ranger") %>%
-set_mode("classification")
+# KNN Model
+knn_model <- nearest_neighbor(neighbors=3) %>%
+  set_mode("classification") %>%
+set_engine("kknn")
 
 # Create recipe
 my_recipe <- recipe(ACTION ~ ., data = train_data) %>%
@@ -46,42 +45,22 @@ my_recipe <- recipe(ACTION ~ ., data = train_data) %>%
 
 
 
-rf_wf <- workflow() %>%
+knn_wf <- workflow() %>%
   add_recipe(my_recipe) %>%
-  add_model(rf_mod)
+  add_model(knn_model)
 
-## Grid of values to tune over
-tuning_grid <- grid_regular(
-  mtry(range = c(1, ncol(train_data) - 1)),
-  min_n(),
-  levels = 3
-)
-
-## Split data for CV
-folds <- vfold_cv(train_data, v = 3, repeats=1)
-
-## Run the CV
-CV_results <- rf_wf %>%
-  tune_grid(resamples=folds,
-            grid=tuning_grid,
-            metrics = metric_set(roc_auc, accuracy))
-
-## Find Best Tuning Parameters
-bestTune <- CV_results %>%
-  select_best(metric = "roc_auc")
 
 ## Finalize the Workflow & fit it
 final_wf <-
-  rf_wf %>%
-  finalize_workflow(bestTune) %>%
+  knn_wf %>%
   fit(data=train_data)
 
 ## Predict
 final_predictions <- final_wf %>%
-  predict(new_data = test_data, type = "prob") %>%
+  predict(knn_wf, new_data=test_data, type="prob") %>%
   bind_cols(test_data %>% select(id)) %>%
   rename(Action = .pred_1) %>%   # Assuming you want P(ACTION = 1)
   select(id, Action)
 
 # Export processed dataset
-vroom_write(x = final_predictions, file = "./amazon_rf.csv", delim = ",")
+vroom_write(x = final_predictions, file = "./amazon_knn.csv", delim = ",")
